c14e8c934dcc270f4d6dda6fc1d59388997a351d
Powell Vince <jvince@sandiego.edu>
2018-10-18 00:36:25 +0000

    changes

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -59,6 +59,16 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+    
+  def similar
+    @movie = Movie.find(params[:id])
+    if @movie.director.blank?
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to '/'
+    else
+      @movies = Movie.same_directors(@movie.director)
+    end
   end
 
 end
